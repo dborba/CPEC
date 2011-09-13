459d92b62c600528442068828f99ac17bb5c4de5
Daniel F. Borba <dborba@gmail.com>
2011-09-13 11:32:14 -0400

    First Commit

---
+++ src/main/java/com/danielborba/cpec/App.java
@@ -0,0 +1,13 @@
+package com.danielborba.cpec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
